3c3fc5299098b28679083988c6ceac04fc7a6112
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-13 23:50:25 +0000

    Agregando mensaje de error

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,35 +1,29 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import "../../styles/index.css"
 
-//create your first component
+
 const Home = () => {
 
 	const [newInput, setNewInput] = useState("");
 	const [tasks, setTasks] = useState(["Lavar los servicios", "Bañar al perro", "Practicar mi logica"]);
-
+	const [isHoverRed, setIsHoverRed] = useState(false)
 
 	function addTask(e){
-		if(e.key === "Enter"){
+		if(e.key === 'Enter'){
 			// setTasks([...tasks, newInput])
 			setTasks(currentTasks => currentTasks.concat(newInput))
 			// setTasks(tasks.concat(newInput))
-			setNewInput('');
+			e.target.value = "";
 		}
 	}
 
 	function deleteButton(index) {
 		setTasks(currentTasks => currentTasks.filter((task, i) => i !== index));
 	}
-	
-	// function deleteButton(e) {
-	// 	if (e.target.classList.contains("delete-btn")) {
-    //         e.target.parentElement.remove();
-    //     }
-	// }
 
+	
 	return (
 		<div className="d-flex justify-content-center align-items-center min-vh-100">
 			<div className="mx-auto w-100 bg-transparent d-flex flex-column p-5 justify-content-center align-items-center ">
@@ -42,13 +36,17 @@ const Home = () => {
 					</div>
 					<div className="w-100 border m-0 p-0">
 						<ul className="list-unstyled w-100 m-0 p-0">
-							{tasks.map((task, index)=>(
-								<li key={index} className="border-bottom p-2 px-5 py-3 text-start d-flex justify-content-between align-items-center fw-lighter fs-3 fst-italic"><span>{task}</span>
-			
-									<FontAwesomeIcon className="delete-btn" icon={faX} style={{color: "red"}} onClick={() => deleteButton(index)}/>
-								
+						{tasks.length === 0 ? (
+      						<li className="text-center w-100 fw-lighter fs-3 fst-italic " style={{color:"purple"}}>No hay tareas, añadir tareas</li>
+    					) : (
+      						tasks.map((task, index) => (task !== "" && (
+								<li key={index} className="border-bottom p-2 px-5 py-3 text-start d-flex justify-content-between align-items-center fw-lighter fs-3 fst-italic">
+									<span>{task}</span>
+									<FontAwesomeIcon className="delete-btn" icon={faX} onClick={() => deleteButton(index)}/>
 								</li>
-							))}
+								)
+							))
+							)}
 						</ul>
 					</div>
 					<div className="w-100 p-3  pb-1">
