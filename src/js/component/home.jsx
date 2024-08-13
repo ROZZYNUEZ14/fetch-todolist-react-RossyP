f43017fd0d84a15575efaae6792e6294c5663ec9
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-13 22:19:43 +0000

    Creando el boton delete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,47 +10,49 @@ const Home = () => {
 	const [newInput, setNewInput] = useState("");
 	const [tasks, setTasks] = useState(["Lavar los servicios", "Bañar al perro", "Practicar mi logica"]);
 
-	
 
 	function addTask(e){
 		if(e.key === "Enter"){
 			// setTasks([...tasks, newInput])
 			setTasks(currentTasks => currentTasks.concat(newInput))
 			// setTasks(tasks.concat(newInput))
-			setNewInput("");
+			setNewInput('');
 		}
 	}
 
-	function deleteButton(e) {
-		if (e.target.classList.contains("delete-btn")) {
-            e.target.parentElement.remove();
-        }
+	function deleteButton(index) {
+		setTasks(currentTasks => currentTasks.filter((task, i) => i !== index));
 	}
+	
+	// function deleteButton(e) {
+	// 	if (e.target.classList.contains("delete-btn")) {
+    //         e.target.parentElement.remove();
+    //     }
+	// }
 
 	return (
 		<div className="d-flex justify-content-center align-items-center min-vh-100">
 			<div className="mx-auto w-100 bg-transparent d-flex flex-column p-5 justify-content-center align-items-center ">
 				<div>
-					<h1>TODO</h1>
+					<h1 className="fw-lighter fst-italic" style={{ fontSize:"100px", color: "#8038b1"}}>ToDo</h1>
 				</div>
-				<div className="w-50">
-					<div className="w-100 p-2 px-5 py-3 border bg-white">
-						<input className="w-100 border-0" type="text" name="" id="" placeholder="Ingresa nueva tarea" onChange={(e)=>setNewInput(e.target.value)} onKeyDown={addTask}/>
+				<div className="w-50 bg-white rounded-3" style={{boxShadow: "5px 10px 10px gray"}}>
+					<div className="w-100 p-2 px-5 py-3">
+						<input className="w-100 border-0 fw-lighter fs-3 fst-italic" style={{outline:"none", color:"#bc7bed"}} type="text" name="" id="" placeholder="Ingresa nueva tarea" onChange={(e)=>setNewInput(e.target.value)} onKeyDown={addTask}/>
 					</div>
 					<div className="w-100 border m-0 p-0">
-						<ul className="list-unstyled w-100 bg-primary m-0 p-0" onClick={deleteButton}>
+						<ul className="list-unstyled w-100 m-0 p-0">
 							{tasks.map((task, index)=>(
-								<li key={index} className="border-bottom p-2 px-5 py-3 text-start d-flex justify-content-between"><span>{task}</span>
-								{/* <span><FontAwesomeIcon className="delete-btn" icon={faX} style={{color: "red"}}/></span> */}
-								
-									<FontAwesomeIcon className="delete-btn" icon={faX} style={{color: "red"}} onClick={deleteButton}/>
+								<li key={index} className="border-bottom p-2 px-5 py-3 text-start d-flex justify-content-between align-items-center fw-lighter fs-3 fst-italic"><span>{task}</span>
+			
+									<FontAwesomeIcon className="delete-btn" icon={faX} style={{color: "red"}} onClick={() => deleteButton(index)}/>
 								
 								</li>
 							))}
 						</ul>
 					</div>
-					<div className="w-100 p-3">
-						<p>{tasks.length}</p>
+					<div className="w-100 p-3  pb-1">
+						<p className="fw-light fst-italic" style={{fontFamily: 'Arial', fontSize:"12px"}}>{tasks.length} {tasks.length === 1 ? "Item" : "Items"} </p>
 					</div>
 				</div>
 				
