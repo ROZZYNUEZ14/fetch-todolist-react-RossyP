79cf559d9ecba9343e94fea50be19a86cbe60d78
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-15 18:00:13 +0000

    Se crea el metodo actualizar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,42 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+
 // import "../../styles/index.css"
 
 
 const Home = () => {
 
 	const [newInput, setNewInput] = useState("");
-	//const [tasks, setTasks] = useState(["Lavar los servicios", "Bañar al perro", "Practicar mi logica"]);
-	const [isHoverRed, setIsHoverRed] = useState(false)
+	const [isHover, setIsHover] = useState(null)
+	const [isInput, setIsInput] = useState(false)
 	const [dataTasks, setDataTasks] = useState([])
+	const [editId, setEditId] = useState()
+	const [editLabel, setEditLabel] = useState()
 
-
-
-	console.log(dataTasks)
 
 	function obtenerHistorialTareas() {
-		fetch(`https://playground.4geeks.com/todo/todos/RossyP`, {
-		  method: "POST",
+		fetch(`https://playground.4geeks.com/todo/users/RossyP`, {
+		  method: "GET",
 		  headers: {
 			"Content-Type": "application/json"
 		  },
-		  body: JSON.stringify({}) 
 		})
-		  .then((response) => response.json())
+		  .then((response) =>{
+			if(response.status === 404){
+				creandoUsuario()
+			}
+			console.log(response)
+			 return response.json()
+			})
 		  .then((data) => {
-			console.log("Tareas recibidas:", data);
-			setDataTasks(currentData => [...currentData, data]); 
+			console.log(data.todos);
+			// setDataTasks(currentData => [...currentData, data.todos]); 
+			setDataTasks(currentData => currentData.concat(data.todos))
 		  })
-		  .catch((error) => console.log("Error al obtener el historial de tareas:", error));
+		  .catch((error) => console.log(error));
 	}
 	
 	function addTask(e){
-		if(e.key === 'Enter'){
-			crearTareas(newInput)
-			// setTasks([...tasks, newInput])
-			//setTasks(currentTasks => currentTasks.concat(newInput))
-			// setTasks(tasks.concat(newInput))
+		if(e.key === 'Enter' || e.type === 'click'){
+			if(editId){
+				editarTareas(editId, editLabel)
+			}else{
+				crearTareas(newInput)
+			}
+			
+			
 			e.target.value = "";
 		}
 	}
@@ -69,7 +80,7 @@ const Home = () => {
 		})
 		.then((response) => response.json())
 		.then((data) => {
-			console.log("Usuario creado:", data);
+			console.log(data);
 			obtenerHistorialTareas();
 		})
 	}
@@ -89,23 +100,72 @@ const Home = () => {
 		.then((data) =>{
 			// setDataTasks(currentData => [...currentData, data]);
 			setDataTasks(currentData => currentData.concat(data));
-			setIdTarea(data.id)
-			setActualTarea(data.label)
 			console.log(data)
 			
 		})
 		.catch((error) => console.log(error))
 	}
 
-
+	function editarTareas(id, label){
+		fetch(`https://playground.4geeks.com/todo/todos/${id}`,{
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				"label": label,
+				"is_done": true
+			})
+		})
+		.then((response) => response.json())
+		.then((data) =>{
+			setDataTasks(currentData => 
+				currentData.map(task => 
+					task.id === id ? { ...task, label: data.label, is_done: data.is_done } : task
+				)
+			);
+			console.log(data)
+		})
+		.catch((error) => console.log(error))
+	}
 	
 
 	useEffect(()=>{
-		creandoUsuario()
+		//creandoUsuario()
 		obtenerHistorialTareas()
 	}, [])
 	
+	function handleClickEdit(id, label) {
+		setEditId(id)
+		setEditLabel(label)
+	}
+
+	console.log(editId)
+	console.log(editLabel)
+
+	function handleChangeEdit(e){
+		setEditLabel(e.target.value)
+	}
+
+	function handleMouseEnter(id) {
+		setIsHover(id)
+	}
+
+	function handleMouseLeave() {
+		setIsHover(null)
+	}
+
+	function handleBlur() {
+		setEditId(null)
+		setEditLabel('')
+	  }
+	function handleMouseLeaveInput(){
+		setIsInput(false)
+	}
 	
+	function handleMouseEnterInput(){
+		setIsInput(true)
+	}
 	return (
 		<div className="d-flex justify-content-center align-items-center min-vh-100">
 			<div className="mx-auto w-100 bg-transparent d-flex flex-column p-5 justify-content-center align-items-center ">
@@ -122,9 +182,21 @@ const Home = () => {
       						<li className="text-center w-100 fw-lighter fs-3 fst-italic " style={{color:"purple"}}>No hay tareas, añadir tareas</li>
     					) : (
 							dataTasks.map((task, index) => (task !== "" && (
-								<li key={task.id } className="border-bottom p-2 px-5 py-3 text-start d-flex justify-content-between align-items-center fw-lighter fs-3 fst-italic">
-									<span>{task.label}</span>
-									<FontAwesomeIcon className="delete-btn" icon={faX} onClick={() => deleteButton(task.id)}/>
+								<li key={task.id } className="border-bottom p-2 px-5 py-3 text-start d-flex justify-content-between align-items-center fw-lighter fs-3 fst-italic" style={{backgroundColor: isHover === task.id ? 'pink' : 'white',}} onMouseEnter={()=>handleMouseEnter(task.id)} onMouseLeave={handleMouseLeave}>
+									{editId === task.id || isInput === true ?(
+											<input value={editLabel} onChange={handleChangeEdit} onKeyDown={addTask} onBlur={handleBlur}></input> 
+										):(
+											<span>{task.label}</span>
+										)
+									}
+									
+									
+									<div className="d-flex flex-row gap-2 justify-content-center align-items-center">
+										<FontAwesomeIcon icon={faFloppyDisk} style={{color: isInput !== null  ? "yellow": "blue"}} onClick={addTask} onMouseEnter={()=>handleMouseEnter(task.id)} onMouseLeave={handleMouseLeaveInput} />	
+										<FontAwesomeIcon className="edit-btn" style={{fontSize: "23px", color: isHover === task.id ? "#575458" : "white"}} icon={faPen} onClick={() => handleClickEdit(task.id, task.label)} />
+										<FontAwesomeIcon className="delete-btn" style={{color: isHover === task.id ? 'red' : 'white',}}  icon={faX} onClick={() => deleteButton(task.id)} />
+										
+									</div>
 								</li>
 								)
 							))
@@ -141,4 +213,5 @@ const Home = () => {
 	);
 };
 
+// onBlur={() => editarTareas(task.id, task.label)}
 export default Home;
